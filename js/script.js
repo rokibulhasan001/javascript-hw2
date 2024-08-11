51c9ef6eec908fb780cfacad6a98144a4010eafc
rokibulhasan001 <164499990+rokibulhasan001@users.noreply.github.com>
2024-08-11 17:34:27 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,124 @@
+
+/* show tex one*/
+function showText5(){
+	document.getElementById('s-text5').innerHTML="My name is Rokibul Hasann";
+}
+/* text change*/
+function myTeam(){
+	document.getElementById('demo').innerHTML="bootstrap.bundle.min.js";
+
+}
+/* text change*/
+function fontChange(){
+	document.getElementById('fontChange').style.fontSize='35px';
+}
+/* text change*/
+function dChange(){
+	document.getElementById('dChange').style.display='none';
+}
+
+/* text change*/
+function hiddenShow(){
+	document.getElementById('hiddenShow').style.display='block';
+}
+/* numberPlus*/
+function numberPlus(){
+	document.getElementById('numberPlus').innerHTML= 500 + 600;
+}
+/* javaScript Statements*/
+let a, b, c;
+a = 5;
+b = 6;
+c = a + b;
+let x, y;
+x = 10;
+y = 20;
+function demo2(){
+	document.getElementById("demo2").innerHTML= c;
+}
+document.getElementById("demo3").innerHTML = c;
+document.getElementById("demo4").innerHTML ="Hello Dolly!";
+
+document.getElementById("demo5").innerHTML = 10.50;
+document.getElementById("demo6").innerHTML = 'Rokibul Hasan';
+document.getElementById("demo7").innerHTML = x;
+document.getElementById("demo8").innerHTML = (5+6) * 10;
+document.getElementById("demo9").innerHTML = x + y;
+document.getElementById("demo10").innerHTML = 5 * 6;
+document.getElementById("demo11").innerHTML = x * 10;
+document.getElementById("demo12").innerHTML = "Rokibul"+""+"Hasan";
+
+// Change heading:
+document.getElementById("myH").innerHTML = " 21.JavaScript Comments";
+// Change paragraph:
+document.getElementById("myP").innerHTML = "My first paragraph.";
+
+/*
+The code below will change
+the heading with id = "myH"
+and the paragraph with id = "myP"
+*/
+document.getElementById("myb").innerHTML = " 22.JavaScript Comments";
+document.getElementById("myc").innerHTML = "My first paragraph.";
+
+x = 30;
+y = 20;
+z = x + y;
+document.getElementById("demo13").innerHTML = "The value of z is: " + z;
+
+var name1 = 3;
+var name2  = 29;
+var  all= name1 + name2;
+document.getElementById("demo14").innerHTML = "The value of all is: " +  all;
+
+
+const name3 = 25;
+const name4 = 25;
+const nameall = name3 + name4;
+document.getElementById("demo15").innerHTML = "The value of nameall " +  nameall;
+
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+document.getElementById("demo16").innerHTML =
+"The total is: " + total;
+
+const pi = 3.14;
+let person = "John Doe";
+let answer = 'Yes I am!';
+
+document.getElementById("demo17").innerHTML =
+pi + "<br>" + person + "<br>" + answer;
+
+let carName = "volvo ", parson = "jone due ", price = 200;
+document.getElementById('demo18').innerHTML = carName + parson;
+
+let t = "9" + 2 + 3 + 4 + 9;
+document.getElementById("demo19").innerHTML = t;
+let m =  2 + 3 + 5+ 2 + "5";
+document.getElementById("demo20").innerHTML = m;
+let $ = 5;
+let $$$ = 55;
+document.getElementById("demo21").innerHTML = $ + $$$;
+let _x = 5;
+let _100 = 7;
+document.getElementById("demo22").innerHTML = _x + _100;
+let text1 = "A";
+let text2 = "B";
+let result = text1 < text2;
+document.getElementById("demo23").innerHTML = "Is A less than B? " + result;
+
+function bulbOn(){
+	document.getElementById('img').src="images/off.gif";
+}
+function bulbOff(){
+	document.getElementById('img').src="images/on.gif";
+}
+function bulbOne(){
+	document.getElementById('img2').src="images/edit3.jpg";
+}
+function bulbOffe(){
+	document.getElementById('img2').src="images/edit4.jpg";
+}
+
+
